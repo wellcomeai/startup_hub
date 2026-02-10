d8cc070358a7152d4b3eaa4729e9016c59e6efc9
wellcomeai <19wellai96@gmail.com>
2026-02-10 19:31:56 +0300

    Update __init__.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from app.api.admin import router as admin_router
 from app.api.auth import router as auth_router
+from app.api.payments import router as payments_router
 from app.api.referrals import router as referrals_router
 from app.api.subscriptions import router as subscriptions_router
 from app.api.users import router as users_router
@@ -11,4 +12,5 @@ api_router.include_router(auth_router)
 api_router.include_router(users_router)
 api_router.include_router(referrals_router)
 api_router.include_router(subscriptions_router)
+api_router.include_router(payments_router)
 api_router.include_router(admin_router)
